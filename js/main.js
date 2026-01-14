2b1fc813583585d3f5385acf8b6f5cb4255f3f55
AntonioGolia-98 <antoniog98@icloud.com>
2026-01-14 10:12:39 +0100

    creazione funzione renderTeamPage e cambiato nome alle variabili img

--- js/main.js
+++ js/main.js
@@ -3,47 +3,47 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    imag: "img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    imag: "img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    imag: "img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    imag: "img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    imag: "img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    imag: "img/female3.png"
   }
 ];
 
 const generateNewCard = ( singleMember ) => {
-    const { name, role, email, img} = singleMember;
+    const { name, role, email, imag } = singleMember;
     const newElement = document.createElement("div");
     newElement.classList.add("team-members");
     newElement.innerHTML = `
         <div class="card-image">
-        <img src="./img/${Img}" alt=" photo of ${name}"> 
+        <img src="${imag}" alt=" photo of ${name}"> 
     </div>
     <div class="card-text">
         <h2>${name}</h2>
@@ -52,3 +52,14 @@ const generateNewCard = ( singleMember ) => {
     </div>`;
     return newElement;
 }
+
+function renderTeamPage ( teamMembers, wrapperId ){
+    const wrapper = document.getElementById(wrapperId);
+
+    for (let i = 0; i < teamMembers.length; i++) {
+        const courentMember = teamMembers[i];
+        wrapper.append(generateNewCard(courentMember));
+    }
+}
+
+renderTeamPage (teamMembers, "Team-container");
